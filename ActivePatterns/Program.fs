8fe54025c12ada2af62990a4d73d3a6cdd052e95
tremorscript <tremorscript@gmail.com>
2024-01-01 09:03:13 +0530

    changes

---
+++ ActivePatterns/Program.fs
@@ -0,0 +1,70 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+// Patterns are parsed and detected dynamically
+let (|Int|_|) str =
+    match System.Int32.TryParse(str: string) with
+    | (true, int) -> Some(int)
+    | _ -> None
+
+
+// create an active pattern
+let (|Bool|_|) str =
+    match System.Boolean.TryParse(str: string) with
+    | (true, bool) -> Some(bool)
+    | _ -> None
+
+// create a funcion to call the patterns
+let testParse str =
+    match str with
+    | Int i -> printfn "The value is an int '%i'" i
+    | Bool b -> printfn "The value is bool '%b'" b
+    | _ -> printfn "The value is '%s' is something else" str
+
+// test
+testParse "12"
+testParse "true"
+testParse "abc"
+
+
+// A similar example is to use active patterns with regular expressions in order to match
+// on a regex pattern and return the matched value in a single step.
+
+// create an active pattern
+open System.Text.RegularExpressions
+
+let (|FirstRegexGroup|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if (m.Success) then Some m.Groups.[1].Value else None
+
+
+// create a function to call the pattern
+let testRegex str =
+    match str with
+    | FirstRegexGroup "http://(.*?)/(.*)" host -> printfn "The value is a url and the host is %s" host
+    | FirstRegexGroup ".*?@(.*)" host -> printfn "The value is an email and the host is %s" host
+    | _ -> printfn "The value '%s' is something else" str
+
+// test
+testRegex "http://google.com/test"
+testRegex "alice@hotmail.com"
+
+// Fizz Buzz challenge
+
+// setup the active patterns
+let (|MultOf3|_|) i =
+    if i % 3 = 0 then Some MultOf3 else None
+
+let (|MultOf5|_|) i =
+    if i % 5 = 0 then Some MultOf5 else None
+
+// the main function
+let fizzBuzz i =
+    match i with
+    | MultOf3 & MultOf5 -> printf "FizzBuzz, "
+    | MultOf3 -> printf "Fizz, "
+    | MultOf5 -> printf "Buzz, "
+    | _ -> printf "%i, " i
+
+// test
+[ 1..20 ] |> List.iter fizzBuzz
---
+++ MapBindApply/Program.fs
@@ -0,0 +1,63 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+// Map is something that takes a function in the normal world and lifts it into an elevated world
+// (a -> b) -> E<a> -> E<b>
+
+// apply unpacks a function wrapped inside an elevated value into a lifted function
+// E<(a->b)> -> E<a> -> E<b>
+
+module Option =
+
+    // the apply function for options
+    let apply fOpt xOpt =
+        match fOpt, xOpt with
+        | Some f, Some x -> Some(f x)
+        | _ -> None
+
+    //infix versions of map and apply
+    let (<!>) = Option.map
+    let (<*>) = apply
+
+    // The lift functions take a normal function with N parameters and transform it to a corresponding elevated function.
+    let lift2 f x y = f <!> x <*> y
+
+    let lift3 f x y z = f <!> x <*> y <*> z
+
+    let lift4 f x y z w = f <!> x <*> y <*> z <*> w
+
+module List =
+
+    // the apply function for lists
+    // [f;g;] apply [x;y] becomes [f x; f y; g x; g y]
+    let apply (flist: ('a -> 'b) list) (xList: 'a list) =
+        [ for f in flist do
+              for x in xList do
+                  yield f x ]
+
+
+//infix version of apply
+let add x y = x + y
+
+let resultOption =
+    let (<*>) = Option.apply
+    (Some add) <*> (Some 2) <*> (Some 3)
+// resultOption = Some 5
+
+let resultList =
+    let (<*>) = List.apply
+    [ add ] <*> [ 1; 2 ] <*> [ 10; 20 ]
+
+
+// If you have apply and return, you can construct map from them
+let resultOption1 =
+    let (<!>) = Option.map
+    let (<*>) = Option.apply
+
+    add <!> (Some 2) <*> (Some 3)
+
+let resultList1 =
+    let (<!>) = List.map
+    let (<*>) = List.apply
+
+    add <!> [ 1; 2 ] <*> [ 10; 20 ]
